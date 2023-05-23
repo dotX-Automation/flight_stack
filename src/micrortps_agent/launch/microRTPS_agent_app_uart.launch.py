840f698c400c8fb80132552492eeb8373cf75bd3
Roberto Masocco <robmasocco@gmail.com>
2023-05-23 11:04:30 +0200

    feat: micrortps_agent: config, launch files
    @lorenzo-bianchi -2

---
+++ src/micrortps_agent/launch/microRTPS_agent_app_uart.launch.py
@@ -0,0 +1,38 @@
+"""
+MicroRTPS Agent App launch file for use with FMU via serial port.
+
+Roberto Masocco <robmasocco@gmail.com>
+Intelligent Systems Lab <isl.torvergata@gmail.com>
+
+May 23, 2023
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Builds a LaunchDescription for the microRTPS Agent App."""
+    ld = LaunchDescription()
+
+    # Build config file path
+    config = os.path.join(
+        get_package_share_directory('micrortps_agent'),
+        'config',
+        'microRTPS_agent_uart.yaml')
+
+    # Create a node action
+    node = Node(
+        package='micrortps_agent',
+        executable='micrortps_agent_app',
+        shell=True,
+        emulate_tty=True,
+        output='both',
+        log_cmd=True,
+        parameters=[config])
+    ld.add_action(node)
+
+    return ld
